7a37cd6cc1932ce69b32a468d620c7cf9af2891b
ChuChucky <slardakoo@gmail.com>
2024-10-31 10:26:49 +0300

    update

--- client/src/pages/Main/MainPage.tsx
+++ client/src/pages/Main/MainPage.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react"
-//import styles from './styles.module.scss'
+import style from './styles.module.scss'
 
 const MainPage:FC = () => {
     return (
         <div className="container">
-           Test123
+            <div className={style.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore explicabo fugit excepturi aspernatur atque suscipit vero animi amet repellat harum autem delectus cum et, ipsam, reprehenderit repudiandae. Animi fugit praesentium, reprehenderit velit, nam vero minus voluptatibus laudantium possimus dolore dolorem ab quis recusandae vel hic temporibus magni ea. Ea, quidem laborum! Voluptate quam quibusdam optio placeat enim dolor, iste odio vero praesentium ducimus sapiente eveniet similique fuga distinctio recusandae atque impedit esse sint amet culpa hic eum. Eligendi perspiciatis laboriosam tempore quos reiciendis sint quidem tempora dolorem officia aliquid aperiam, veritatis atque inventore facere debitis. Id ab error autem accusantium.</div>
         </div>
     );
 };
